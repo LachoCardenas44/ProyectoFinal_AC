d6cfc4392041f83b3ef62b3d6d5f9cfcde7514ce
LachoCardenas44 <116188859+LachoCardenas44@users.noreply.github.com>
2023-07-12 17:25:53 -0600

    Better

--- fibo.asm
+++ fibo.asm
@@ -2,8 +2,15 @@ include 'emu8086.inc'
 
 datos segment
     
-    arreglo db 64 dup(?) 
+    salva1 dw ?
+    salva2 dw ?
     unidad db ?
+    u1 db ?
+    u2 db ?
+    u3 db ?
+    u4 db ?
+    u5 db ?
+    
 
  
     x db 2
@@ -29,16 +36,9 @@ codigo segment
         call clear_screen
         gotoxy x,y
         
-        mov si,0
-        mov di,1 
-        mov cx,0
-        mov bp,0
-        mov arreglo[si],0
-        mov arreglo[di],1
-        
         
         call pthis
-        db "Introduzca un puto numero: ",0
+        db "Introduzca un numero: ",0
           
           
         ;------------------------
@@ -72,35 +72,59 @@ codigo segment
          
         
         goto:
-        mov al,dl
-        mov cl,al       
-        mov y,4      
+        mov u1,dl      
+         mov y,4
         gotoxy x,y
         
+        mov ah,02h
+        mov dl,'0'
+        int 21h
+        mov ah,02h
+        mov dl,'0'
+        int 21h
+        mov ah,02h
+        mov dl,' '
+        int 21h 
+        
+       
+                 
+                 
+                 
+        mov ah,0
+        mov al,bl
+        mov ch,0
+        mov cl,u1
+        dec cl
+        xor dx,dx
+        mov dx,0h 
+        mov bx,01h 
         
         ciclo:
-            cmp al,0
-            je terminar_ciclo 
-            mov bl,arreglo[di]
-            add bl,arreglo[si]
-            inc si
-            inc di 
-            mov arreglo[di],bl
-            dec al
             
-        jmp ciclo
-                   
-        terminar_ciclo:  
+            mov salva1,bx
+            add bx,dx
+            mov salva2,bx 
+            cmp bx,2AC2h
+            jge special_print2
+            cmp bx,1000d
+            jg special_print1
+            cmp bx,179h
+            jge special_print
+            cmp bx,144d
+            jge imprimir_3_digitos
+            jmp imprimir_2_digitos 
+            
+            salto:
+            
+                        
+        loop ciclo
         
-              cmp cl,0
-              je game_over
-              
-              mov al,arreglo[bp]
-              cmp ax,144d
-              jge imprimir_3_digitos
+                
+              jmp game_over
+                 
               
               imprimir_2_digitos:
-              
+              mov ax,bx
               aam
               mov bx,ax
               add bx,3030h
@@ -115,14 +139,18 @@ codigo segment
               
               mov ah,02h
               mov dl,' '
-              int 21h            
+              int 21h 
+              
+              mov bx,salva2
+            mov dx,salva1           
                           
-              jmp avanza   
+              jmp salto   
               
               
               imprimir_3_digitos:
-              aam
               
+              mov ax,bx
+              aam           
               add al,30h
               mov unidad,al
               
@@ -134,7 +162,7 @@ codigo segment
               mov bx,ax
               add bx,3030h
                              
-               mov ah,02h 
+              mov ah,02h 
               mov dl,bh
               int 21h 
               
@@ -148,17 +176,160 @@ codigo segment
               
               mov ah,02h
               mov dl,' '
+              int 21h 
+              
+              mov bx,salva2
+            mov dx,salva1 
+              
+              jmp salto
+              
+              special_print:
+              xor dx,dx
+              mov ax,bx
+              mov bx,10
+              div bx
+              mov u3,dl
+              xor dx,dx
+              div bx
+              mov u2,dl
+              xor dx,dx
+              div bx
+              mov u1,dl  
+              xor dx,dx
+              xor ax,ax
+              
+              mov al,u1
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u2
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u3
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              
+              mov ah,02h
+              mov dl,' '
+              int 21h
+              
+              mov bx,salva2
+            mov dx,salva1
+              
+              jmp salto
+              
+              
+              
+              
+              special_print1:
+              xor dx,dx              
+              mov ax,bx
+              mov bx,10
+              div bx
+              mov u4,dl
+              xor dx,dx
+              div bx
+              mov u3,dl
+              xor dx,dx
+              div bx
+              mov u2,dl
+              xor dx,dx
+              div bx
+              mov u1,dl
+              xor dx,dx
+              xor ax,ax
+              
+              mov al,u1
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u2
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u3
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u4
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              
+              mov ah,02h
+              mov dl,' '
               int 21h  
               
-              avanza:
+              mov bx,salva2
+            mov dx,salva1
               
-              inc bp 
-              dec cl              
+              jmp salto
+            
+              special_print2:
               
+              xor dx,dx              
+              mov ax,bx
+              mov bx,10
+              div bx
+              mov u5,dl
+              xor dx,dx
+              div bx
+              mov u4,dl
+              xor dx,dx
+              div bx
+              mov u3,dl
+              xor dx,dx
+              div bx
+              mov u2,dl
+              xor dx,dx
+              div bx
+              mov u1,dl
+              xor dx,dx
+              xor ax,ax
               
-        jmp terminar_ciclo  
-        
-        
+              mov al,u1
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u2
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u3
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u4
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              mov al,u5
+              add al,30h
+              mov ah,02h
+              mov dl,al
+              int 21h
+              
+              mov ah,02h
+              mov dl,' '
+              int 21h  
+              
+              mov bx,salva2
+            mov dx,salva1
+              
+              jmp salto
         
         
                               
@@ -177,7 +348,9 @@ ret
 main endp
     codigo ends
 
-define_pthis    
+define_pthis 
+define_print_num
+define_print_num_uns   
 define_clear_screen
 
 end main
